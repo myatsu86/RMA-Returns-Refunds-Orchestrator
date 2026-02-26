fab21e2ccb7bc9c0cf34aa4eff26a51555a28949
myatsu86 <myat86@gmail.com>
2026-02-26 23:27:03 +0100

    Fixed wrong postgresql password

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://app:app_pwd@localhost:5432/rma_db"
+DATABASE_URL = "postgresql://app:app_pw@localhost:5432/rma_db"
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
